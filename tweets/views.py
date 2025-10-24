77947353c8e47b5ac59c835dc46461c7d45a2dca
rdevang71 <rdevang71@gmail.com>
2025-10-24 19:15:31 +0530

    feat: tweet_edit

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
-from django.shortcuts import get_list_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 
 def index(request):
@@ -22,4 +22,18 @@ def tweet_create(request):
             return redirect('tweet_list')
     else:
         form = TweetForm()
+    return render(request, 'tweet_from.html',{'form':form})
+
+
+def tweet_edit(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
+    if request.method =='POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else: 
+        form = TweetForm(instance=tweet)
     return render(request, 'tweet_from.html',{'form':form})
