bd675374982deb637b760e45ea0ad8b804ba362c
rdevang71 <rdevang71@gmail.com>
2025-10-24 21:52:50 +0530

    improved navbar

--- tweets/views.py
+++ tweets/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -11,7 +12,7 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render(request, 'tweet_list.html',{'tweets':tweets})
 
-
+@login_required
 def tweet_create(request):
     if request.method== "POST":
         form = TweetForm(request.POST, request.FILES)
@@ -25,6 +26,7 @@ def tweet_create(request):
     return render(request, 'tweet_from.html',{'form':form})
 
 
+@login_required
 def tweet_edit(request,tweet_id):
     tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
     if request.method =='POST':
@@ -38,6 +40,8 @@ def tweet_edit(request,tweet_id):
         form = TweetForm(instance=tweet)
     return render(request, 'tweet_from.html',{'form':form})
 
+
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet , pk = tweet_id, user = request.user)
     if request.method == 'POST':
