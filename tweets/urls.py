168ac5a50b3cbf3e045b651d62ae33decc8998f1
rdevang71 <rdevang71@gmail.com>
2025-10-24 22:09:02 +0530

    added authentication system

--- tweets/urls.py
+++ tweets/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('create/', views.tweet_create, name='tweet_create'),
     path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
     path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
-
+    path('login/', views.login_page, name='login_page'),
+    path('logout/', views.logout_page, name='logout_page'),
+    path('register/', views.register_page, name='register_page'),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,9 @@ from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 # Create your views here.
 
 def index(request):
@@ -49,3 +52,50 @@ def tweet_delete(request, tweet_id):
         return redirect('tweet_list')
     return render(request, 'tweet_confirm_delete.html',{'tweet':tweet})
     
+
+
+def register_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already taken.")
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, "Email already registered.")
+        else:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            messages.success(request, "Account created successfully. Please log in.")
+            return redirect("login_page")
+
+    return render(request, "register.html")
+
+def login_page(request):
+    if request.method == "GET" and request.GET.get('next'):
+        messages.error(request, "You need to Register/Login to access that page.")
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user_obj = User.objects.get(email=email)
+            user = authenticate(request, username=user_obj.username, password=password)
+
+            if user is not None:
+                login(request, user)
+                next_url = request.GET.get('next') or 'tweet_list'
+                messages.success(request, "Login successful.")
+                return redirect(next_url)
+            else:
+                messages.error(request, "Invalid credentials.")
+        except User.DoesNotExist:
+            messages.error(request, "No account found with that email.")
+
+    return render(request, "login.html")
+
+def logout_page(request):
+    logout(request)
+    return redirect('login_page')
